5abfe247b3744d1dc290dff40a2f55b7f8f9dd34
Nelson <mandelarom828@gmail.com>
2022-06-02 05:52:11 -0700

    Modify file : 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,7 @@ int main(void)
 	/*Main function; This is the starting point of this program's execution.*/
 {
 /*Initialize str variable*/
-	char str[]="\"Programming is like building a multilingual puzzle \n";
+	char str[53]="\"Programming is like building a multilingual puzzle \n";
 
 	puts(str);
 /*Return : Always 0 (Program executed successfully)*/
