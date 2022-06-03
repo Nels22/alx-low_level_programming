4a00d2b83007c6e7aa2d869364f81f1dabc31f8b
Nelson <mandelarom828@gmail.com>
2022-06-03 12:33:58 -0700

    Add file : 4-print_alphabt

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,7 +1,6 @@
 #include <stdlib.h>                                
 #include <time.h>                                    
 #include<stdio.h>                                    
-
 /**                                                  
  *
  * main - Prints all alphabets except q and e
@@ -10,15 +9,14 @@
  *                                                  
  **/                                                 
 int main(void)                                       
-{                                                   
+{
 /*Declaration of variables*/     
-       	char alph[24] = "abcdfghijklmnoprstuvwxyz";       
-       	int i;                                       	                        
-
-	for (i = 0; i < 24 ; i++)                    
-  	{                                            
-		putchar(alph[i]);                    
-	}                                            
-        putchar('\n');                               
-	return (0);                                  			                                  
+	char alph[24] = "abcdfghijklmnoprstuvwxyz";
+       	int i;
+	for (i = 0; i < 24 ; i++)
+	{
+		putchar(alph[i]);
+	}
+	putchar('\n');
+	return (0);
 }                                   
