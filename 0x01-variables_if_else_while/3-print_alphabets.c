ddbb30aa5c409878aee1a40f8d93c12679bed068
Nelson <mandelarom828@gmail.com>
2022-06-03 11:48:16 -0700

    Add file:3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,7 +2,7 @@
 #include <time.h>
 #include<stdio.h>
 /**
- * main - Prints alphabet in lowercase
+ * main - Prints alphabet in lowercase and uppercase
  *
  * Return: Always 0 (Success)
  *
@@ -12,11 +12,13 @@ int main(void)
 {
 /*Declaration of variables*/
 	char alph[26] = "abcdefghijklmnopqrstuvwxyz";
+	char ALPH[26] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	int i;
 
 	for (i = 0; i < 26 ; i++)
 	{
 		putchar(alph[i]);
+		putchar(ALPH[i]);
 	}
 	putchar('\n');
 	return (0);
