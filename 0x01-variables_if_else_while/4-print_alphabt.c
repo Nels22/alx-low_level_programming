6b19f8c4079fcfbff5bd2b345724334046433d9b
Nelson <mandelarom828@gmail.com>
2022-06-03 16:23:28 -0700

    Update : 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -4,17 +4,18 @@
 /**
  * main - prints all lowercase alphabets except q and e
  *
- * Return : Always 0 (Success)
+ * Return: Always 0 (Success)
  *
 **/
-int main (void)
+int main(void)
 {
 	char alph[24] = "abcdfghijklmnoprstuvwxyz";
 	int i;
+
 	for (i = 0; i < 24 ; i++)
 	{
 		putchar(alph[i]);
 	}
 	putchar('\n');
 	return (0);
-}                                   
+}	
