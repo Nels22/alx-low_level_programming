8d1aae575b0f044d5d1f5857ef407ae79e535780
Nelson <mandelarom828@gmail.com>
2022-06-07 13:06:08 -0700

    ADD FILES:2-alphabet_x10 2-print_alphabet_x10.c | UPDATE FILE: main.h

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - function prints lowercase letters 
+ * of the alphabet 10 times followed by new line
+ * Return:0 success
+ */
+void print_alphabet_x10(void)
+{
+	/*Declaration of local variables*/
+	int ch, i;
+	for (i = 0; i <= 10; i++)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+		_putchar('\n');
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,8 +2,9 @@
  * This is the main header file for my functions
  *
  **/
-int _putchar(char c);
 
+int _putchar(char c);
 void print_alphabet(void);
+void print_alphabet_x10(void);
 
 
