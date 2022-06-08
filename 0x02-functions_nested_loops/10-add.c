4f44c1cd8bf316b248d2f8817f78e704e0e9abf9
Nelson <mandelarom828@gmail.com>
2022-06-07 23:38:47 -0700

    Add 10-add.c files and Update main.h

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * add - This function adds 2 integers and returns the result
+ * @a: First operand
+ * @b: Second operand
+ */
+
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+
+	return (sum);
+}
