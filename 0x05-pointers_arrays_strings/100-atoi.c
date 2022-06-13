ce2885e97f8dff31a1a23870faa967321930fb00
Nels22 <mandelarom828@gmail.com>
2022-06-13 23:52:37 +0000

    Make corrections to the 100-atoi.c to reflect Betty style code

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -14,13 +14,13 @@ int _atoi(char *s)
 
 	negative = 1;
 	i = num = 0;
-	while((s[i] < '0' || s[i] > '9') && s[i] != 0)
+	while ((s[i] < '0' || s[i] > '9') && s[i] != 0)
 	{
 		if (s[i] == '-')
 			negative = negative * -1;
 		i++;
 	}
-	while ((s[i] >= '0' && s[i] <='9') && s[i] != 0)
+	while ((s[i] >= '0' && s[i] <= '9') && s[i] != 0)
 	{
 		if (num >= 0)
 		{
